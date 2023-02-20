8cf2898c42d1fe57711fb148ec6904fc7190d00b
Nap199I <nathan_parry15@hotmail.com>
2023-02-20 12:05:39 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     let buttons = document.getElementsByTagName('button');
 
     for (let button of buttons) {
+
         button.addEventListener('click', function() {
+
             if (this.getAttribute('data-type') === 'submit') {
                 alert('You clicked submit!');
+
             } else {
+
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
+
             }
         })
     }
+
+    runGame('addition');
 })
 
 /**
@@ -18,10 +26,19 @@ document.addEventListener('DOMContentLoaded', function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
+
     // creating 2 random numbers between 1 and 25
+
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `Unkown game type ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -40,7 +57,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 
 }
 
